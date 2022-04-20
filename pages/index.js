e3943fb5375f3a76cc065959b2811c164e36b1cf
Andrés Felipe Jaimes Sánchez <andres_ja_sa@outlook.com>
2022-04-19 22:31:14 -0500

    refactor: progress examples

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
+import Card from '../components/Cards/Card/Card';
 import ScoreCard from '../components/Cards/ScoreCard/ScoreCard';
+import LoaderCircle from '../components/Loaders/LoaderCircle/LoaderCircle';
+import css from '../styles/Styles.module.css';
 
 export default function Home() {
   return (
-    <div className="container" style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
-      <ScoreCard score={1} />
-      <ScoreCard score={1005} />
+    <div className={css.root}>
+      <Card>
+        <span className={css.title}>progress bar test</span>
+        <LoaderCircle percentage={90} title="90%" />
+      </Card>
+      <ScoreCard score={100} />
       <ScoreCard score={0} />
       <ScoreCard score={14564} />
     </div>
